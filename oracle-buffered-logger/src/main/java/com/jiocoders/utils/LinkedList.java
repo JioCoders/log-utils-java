b1a059028987ef839ac94a363e240841afbb0b79
JioCoders <jiocoders@gmail.com>
2026-07-26 13:52:40 +0530

    OBL-1|JIO|enhance

---
+++ oracle-buffered-logger/src/main/java/com/jiocoders/utils/LinkedList.java
@@ -0,0 +1,17 @@
+package com.jiocoders.utils;
+
+/**
+ * "Sum Type" representation of a linked list.
+ */
+public sealed interface LinkedList<T> {
+
+    /**
+     * An empty list.
+     */
+    record Empty<T>() implements LinkedList<T> {}
+
+    /**
+     * A not empty list.
+     */
+    record NotEmpty<T>(T first, LinkedList<T> rest) implements LinkedList<T> {}
+}
